121753dc7a4bfa716ef64621b612842d500f1f72
mmothiba4 <mmothiba4@gmail.com>
2023-07-21 18:36:46 +0200

    hello world

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-number = float(input("Input a number:-10, 10"))
+number = float(input("Input a number:-10"))
 if number > 0:
     print("is positive")
 
