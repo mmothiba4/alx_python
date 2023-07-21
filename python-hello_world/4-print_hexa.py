19b4279195c9ca9910caa523bffd8b40926ad525
mmothiba4 <mmothiba4@gmail.com>
2023-07-21 14:52:51 +0200

    hello world

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,6 +1,10 @@
 #!/usr/bin/bash/python3
-for number in range(99)
-print("{:0x}".format(number), end="\n ")
+numbers = range(99)
+for number in range(99):
+    print("{:0x}".format(number), end="\n ")
+    
+
+
 
 
       
