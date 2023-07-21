6ed687019841d8a3e5ef6616744e8efe35f05a36
mmothiba4 <mmothiba4@gmail.com>
2023-07-21 19:17:31 +0200

    hello world

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number)%10
+last_digit = abs(number)%-10
 if number < 0:
     last_digit = -last_digit
     print("Last digit of", number, "is", last_digit, end=" ")
@@ -14,4 +14,3 @@ elif last_digit == 0:
 
 else:
     print("and is less than 6 and not 0")
-    
