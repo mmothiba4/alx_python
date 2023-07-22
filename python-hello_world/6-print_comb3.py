c0f0e24a04a3d68163d258faf515694c52907475
mmothiba4 <mmothiba4@gmail.com>
2023-07-22 06:27:05 +0200

    hello world

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 9):
     for j in range(i + 1, 10):
         if i < 8:
-            print("{:d}{:d}".format(i, j))
+            print("{:d}{:d}".format(i, j), end=", ")
         else:
             print("{:d}{:d}".format(i, j))
