93090ecaf1384bdd21c19226308496c92439309f
mmothiba4 <mmothiba4@gmail.com>
2023-07-22 16:30:48 +0200

    functions

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 def is_prime(number):
-    for i in range(2, int(number/2)):
+    for i in range(3, number):
         if (number%i) == 0:
             return False
+        else:
+            return False
+        
         return True
+    print is_prime(3)
     
