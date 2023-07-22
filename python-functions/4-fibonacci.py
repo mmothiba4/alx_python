8c5ce6c0b1b1ad907648166de93f1687b92f2df0
mmothiba4 <mmothiba4@gmail.com>
2023-07-22 12:34:45 +0200

    functions

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,4 +2,5 @@
 def fibonacci_sequence(n):
     if n in {0, 1}:
         return n
-    return fibonacci_sequence(n - 1) + fibonacci_sequence(n - 2)
+    else:
+        return(fibonacci_sequence(n - 1) + fibonacci_sequence(n - 2))
