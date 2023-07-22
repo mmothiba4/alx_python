1314c5eda426a83ad6f2df6f23696d7cb3505741
mmothiba4 <mmothiba4@gmail.com>
2023-07-22 06:31:57 +0200

    hello world

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-numbers = range(99)
-for number in range(99):
-    print("{:d} = 0x{:x)}".format(number, number))
+for number in range(0, 99):
+    print("{:d} = 0x{:x}".format(number, number))
 
     
 
