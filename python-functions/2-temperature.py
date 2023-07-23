d8888c1ae711ef929da834fb10643391d4f97e18
mmothiba4 <mmothiba4@gmail.com>
2023-07-23 12:53:13 +0200

    functions

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 def convert_to_celsius(fahrenheit):
     fahrenheit = float(input())
-    C = (5 / 9) * (fahrenheit - 32)
-    return C
+    celsius = (5 / 9) * (fahrenheit - 32)
+    return celsius
 
