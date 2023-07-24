8d0468cebb15ff67cf66aa979fd8524530d41623
mmothiba4 <mmothiba4@gmail.com>
2023-07-24 20:35:29 +0200

    functions

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 def fibonacci_sequence(n):
     if n <= 0:
-        return []
-    if n == 1:
-        return [0]
-    fibonacci_sequence = [0, 1]
-    if n == 2:
-        return [1]
-    for i in range(2, n):
-        fibonacci_sequence.append(fibonacci_sequence[i-1] + fibonacci_sequence[i-2])
-        return []
+        return -1
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a = 0
+        b = 1
+        result = 0
+        for i in range(2, n):
+        result = a + b
+        a = b
+        b = result
+        return result
         
 
 
