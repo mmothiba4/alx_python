0f6ea2734bb7e1bbf6f807c8d7ee2b4fec18c981
mmothiba4 <mmothiba4@gmail.com>
2023-07-24 15:53:04 +0200

    functions

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 def fibonacci_sequence(n):
-    if n < 0:
-        print("incorrect input")
-    elif n == 1:
-        return 0
-    elif n == 2:
-        return 1
+    if n in {0, 1}:
+        return n
     else:
         return(fibonacci_sequence(n - 1) + fibonacci_sequence(n - 2))
