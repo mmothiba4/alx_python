071a633da4b75a96b9f33f09dd359a09b074abda
mmothiba4 <mmothiba4@gmail.com>
2023-07-24 21:02:58 +0200

    function

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 def fibonacci_sequence(n):
-    if n <= 0:
-        return []
+    if n == 0:
+        return [0]
     elif n == 1:
-        return 0
-    elif n == 2:
-        return result
-    for i in range(2, n):
-        result.append(result[i-1] + result[i-2])
-    return result
+        return [0, 1]
+    else:
+        list = fibonacci_sequence(n-1)
+        list.append(list[-1] + list[-2])
+        return list
+    
     
         
         
