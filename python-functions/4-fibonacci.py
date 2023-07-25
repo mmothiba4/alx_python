382bd8898a0df4c2b97ef261a0ce2244d4288d12
mmothiba4 <mmothiba4@gmail.com>
2023-07-25 09:32:04 +0200

    functions

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -9,7 +9,7 @@ def fibonacci_sequence(n):
     fibonacci_sequence = [0, 1]
     for i in range(2, n):
         fibonacci_sequence.append(fibonacci_sequence[i-1] + fibonacci_sequence[i-2])
-        return []
+        return fibonacci_sequence
     
     
         
