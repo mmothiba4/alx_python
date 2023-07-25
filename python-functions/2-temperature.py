d988231fca0a37d525c515a70dfe09e42e17e361
mmothiba4 <mmothiba4@gmail.com>
2023-07-25 09:57:48 +0200

    functions

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 def convert_to_celsius(fahrenheit):
-    
+    fahrenheit = float(input("enter the temperature in fahrenheit: "))  
     celsius = (fahrenheit - 32) / 1.8
     return celsius
-fahrenheit = float(input("enter the temperature in fahrenheit: "))
-print("Temperature in fahrenheit = {:.2f}".format(fahrenheit))
-print("Temperature in celsius = {:.2f}".format(convert_to_celsius(fahrenheit)))
+
 
