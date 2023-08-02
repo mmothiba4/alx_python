f8438de3ae9ccfdd3cd7fc5012e3651a4cf48a3d
mmothiba4 <mmothiba4@gmail.com>
2023-08-02 15:12:29 +0200

    classes

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""class square that defines a square"""
+"""class Square that defines a square"""
 
-class square: 
-    """class square that defines a square"""
+class Square: 
+    """class Square that defines a square"""
     def __init__(self, size):
         """initialize square
         Args:
