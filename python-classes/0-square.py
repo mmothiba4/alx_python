028542c684ea5e31177a939303932470c7ab26ed
mmothiba4 <mmothiba4@gmail.com>
2023-08-02 15:01:48 +0200

    classes

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class square that defines a square"""
+
+class square: 
+    """class square that defines a square"""
+    def __init__(self, size):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size  #: size of the square
+    
