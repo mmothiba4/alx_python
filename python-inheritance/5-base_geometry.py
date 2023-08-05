744faaf07fda4614184c5563a5720d556250f9ef
mmothiba4 <mmothiba4@gmail.com>
2023-08-05 21:21:41 +0200

    inheritance

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,8 +4,29 @@
 
 class BaseGeometry():
     """BaseGeometry class"""
+
+class BaseGeometryMeta(type):
+     
+    """BaseGeometry's meta class"""
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+    """empty class"""
+    pass
+    
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
+    """BaseGeometry metaclass"""
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+    """empty class"""
+    pass
+
     def area(self):
-        """Method Raises an Exception"""
+        """Raises an Exception"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
