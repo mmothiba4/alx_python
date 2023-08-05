5cb467d4e92a102264d1bc7b0902e4d8d679b6bb
mmothiba4 <mmothiba4@gmail.com>
2023-08-05 17:57:49 +0200

    inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,6 +2,13 @@
 """Defines an empty class BaseGeometry."""
 
 
-class BaseGeometry():
-    """Represent base_geometry."""
+class BaseGeometry:
+    """Represent base geometry."""
     pass
+
+#define object
+obj = BaseGeometry()
+print(obj)
+#set an attribute of the object for the empty class
+obj.title = 'Empty Class'
+print(obj.title)
