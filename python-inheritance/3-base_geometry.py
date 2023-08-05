77b0f5d672d2ab54bc2fc8cf310cdf43a5763593
mmothiba4 <mmothiba4@gmail.com>
2023-08-05 19:04:54 +0200

    inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
 """
-Defines an empty class BaseGeometry.
+Defines an empty class BaseGeometry
 """
 
 
-class BaseGeometry:
-    """An empty class"""
-    pass
+class BaseGeometryMeta(type):
+    """Base geometry's meta class"""
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
 
-#define object
-bg = BaseGeometry()
-print(bg)
-print(dir(bg))
-print(dir(BaseGeometry))
+class BaseGeometry(metaclass= BaseGeometryMeta):
+    """Description of the class"""
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+    
+    bg = BaseGeometry()
+    print(dir(bg))
+    print()
+    print(dir(BaseGeometry))
