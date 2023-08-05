035ec873e2042e704f03949b310db82cf745145c
mmothiba4 <mmothiba4@gmail.com>
2023-08-05 21:29:33 +0200

    inheritance

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,6 +1,40 @@
 #!/usr/bin/python3
-"""Rectangle class Module"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""BaseGeometry class Module"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+
+class BaseGeometryMeta(type):
+     
+    """BaseGeometry's meta class"""
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+    """empty class"""
+    pass
+    
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
+    """BaseGeometry metaclass"""
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+    """empty class"""
+    pass
+
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method for validating value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
