8c0ec9aa9d3d0ebd63f34648f761ddbc37103593
mmothiba4 <mmothiba4@gmail.com>
2023-08-05 21:06:40 +0200

    inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -12,14 +12,7 @@ class BaseGeometry:
 
 class BaseGeometryMeta(type):
      
-    """
-    BaseGeometry's meta class
-    """
-    def __dir__(self):
-        attributes = super().__dir__()
-        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
-        return new_attribute_list
-    """empty class"""
+    """BaseGeometry's meta class"""
     pass
     
 
