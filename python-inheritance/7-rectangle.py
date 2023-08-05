f82a0656526019c5c9c6e5083824db6a5fb2951c
mmothiba4 <mmothiba4@gmail.com>
2023-08-05 21:49:06 +0200

    inheritance

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,16 +1,50 @@
 #!/usr/bin/python3
-"""Rectangle class Module"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+"""BaseGeometry class Module"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+
+class BaseGeometryMeta(type):
+     
+    """BaseGeometry's meta class"""
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+    """empty class"""
+    pass
+    
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
+    """BaseGeometry metaclass"""
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+    """empty class"""
+    pass
+
+    def area(self):
+        """Raises an Exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Method for validating value"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
     """Rectangle class"""
+
     def __init__(self, width, height):
         """Initilize rectangle method"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+        self.__width = super().integer_validator("width", width)
+        self.__height = super().integer_validator("height", height)
+
 
     def area(self):
         """Method that returns area of rectangle"""
