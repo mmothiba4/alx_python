a175741e07a2554bea15178d22c99a80d94b49ad
mmothiba4 <mmothiba4@gmail.com>
2023-08-05 20:50:47 +0200

    inheritance

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -9,12 +9,34 @@ class BaseGeometry:
     Empty class
     """
     pass
+
+class BaseGeometryMeta(type):
+     
+    """
+    BaseGeometry's meta class
+    """
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+    
+
+class BaseGeometry(metaclass=BaseGeometryMeta):
+    """
+    Description of the class
+    """
+
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
+
     
 """Description of the object"""
     
-bg = BaseGeometry()
+a = BaseGeometry()
 
-print(dir(bg))
+print(dir(a))
 
 print()
 
