a39b20f868753bd85dce880defebdd4ed3ebdb20
mmothiba4 <mmothiba4@gmail.com>
2023-08-16 12:48:48 +0200

    almost a circle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" class Square that defines a square"""
+"""class Square that defines a square.
+"""
 
 
 class Square:
-    """ class Square that defines a square"""
+    """Class Square that defines a square.
+    """
 
     def __init__(self, size=0):
-        """initialize square"""
+        """Initialize a new square.
+        """
         if type(size) is not int:
             raise TypeError('size must be an integer')
         elif size < 0:
