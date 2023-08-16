66eca8415067252aab8eab59e3b00e6ba2e6c7f4
mmothiba4 <mmothiba4@gmail.com>
2023-08-16 14:19:45 +0200

    almost a circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,9 +1,22 @@
 #!/usr/bin/python3
-"""Defines a base model class."""
+"""Defines a rectangle class that inherits from base class."""
 
 class Rectangle(Base):
-    pass
+    __nb_objects = 0
 
+    def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+"""Defines a base model class."""
 
 class Base:
     """Represent the base model.
