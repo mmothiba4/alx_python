b315ce75c8541e8f46d2ad824c0f20ef87e124f9
mmothiba4 <mmothiba4@gmail.com>
2023-08-18 13:32:57 +0200

    network

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-""" Script that takes in a URL and an email, sends a POST request
- And displays the body of the response
+"""  script that takes in a URL and an email, sends a POST request to the
+    passed URL with the email as a parameter, and displays the body of the
+    response
 """
-
-import sys
 import requests
+from sys import argv
+
+
+def post_emailr():
+    """sends a POST request to the passed URL with the email as a parameter"""
+    url = argv[1]
+    values = {'email': argv[2]}
+    r = requests.post(url, data=values)
+    print(r.text)
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as resp:
-        print(resp.read().decode("utf-8"))
+    post_emailr()
