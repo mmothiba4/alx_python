124fd9ea8eb09f170e207b60e665cf5088fe4aa0
mmothiba4 <mmothiba4@gmail.com>
2023-08-18 14:25:33 +0200

    network

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -4,21 +4,16 @@
     response
 """
 import requests
-import sys
+from sys import argv
 
 
-
-def post_email():
+def post_emailr():
     """sends a POST request to the passed URL with the email as a parameter"""
     url = argv[1]
     values = {'email': argv[2]}
-    data = urllib.parse.urlencode(values)
-    data = data.encode('utf-8')  # data should be bytes
-    req = urllib.request.Request(url, data)
-    with urllib.request.urlopen(req) as response:
-        the_page = response.read().decode('utf-8')
-    print(the_page)
+    r = requests.post(url, data=values)
+    print(r.text)
 
 
 if __name__ == "__main__":
-    post_email()
+    post_emailr()
