682c0a96c4cee83f98b6ea72cad189a622606613
mmothiba4 <mmothiba4@gmail.com>
2023-08-18 14:17:41 +0200

    network

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""A script that takes in a URL,
-Sends a request to the URL,
-And displays the value of the X-Request-Id
-variable found in the header ofthe response.
+"""  script that takes in a URL, sends a request to the URL and displays
+    the value of the X-Request-Id variable found in the header of the response.
 """
 
-import sys
+
 import requests
+from sys import argv
 
 
-def request_id():
+def request_idr():
     """ displays the value of the X-Request-Id variable """
-    with urllib.request.urlopen(argv[1]) as response:
-        the_page = response.info()
-    print(the_page['X-Request-Id'])
+    req = requests.get(argv[1])
+    print(req.headers.get('X-Request-Id'))
 
 
 if __name__ == "__main__":
-    request_id()
+    request_idr()
