4548443b2df265eeb746e5181d2b06487957476d
mmothiba4 <mmothiba4@gmail.com>
2023-08-29 18:09:46 +0200

    web framework

---
+++ python-web_framework/1-hbnb_route.py
@@ -0,0 +1,18 @@
+"""A script that starts a Flask web application:
+listening on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+app = Flask (__name__)
+
+@app.route("/")
+def hello_HBNB():
+    return "Hello HBNB!"
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")
+    
