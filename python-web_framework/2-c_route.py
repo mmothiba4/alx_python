23da86a01a514b8bc1487854a1996fa43d7b886c
mmothiba4 <mmothiba4@gmail.com>
2023-08-31 09:39:49 +0200

    web framework

--- python-web_framework/2-c_route.py
+++ python-web_framework/2-c_route.py
@@ -18,7 +18,7 @@ def hbnb():
 """A function that returns a specified string when returning the text in that directory."""
 @app.route("/c/<text>")
 def c_is_fun():
-    return f"C {escape(is super fun)}"
+    return f"c {escape(c is fun)}"
 
 
 if __name__ == "__main__":
