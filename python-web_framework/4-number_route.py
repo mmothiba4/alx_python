4ef64d7a1f1583d2f2f4842179d13698e296f028
mmothiba4 <mmothiba4@gmail.com>
2023-08-31 13:32:51 +0200

    web framework

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,34 @@
+"""A script that starts a Flask web application:
+listening on 0.0.0.0, port 5000"""
+
+from flask import Flask
+
+app = Flask (__name__)
+
+@app.route("/")
+def hello_HBNB():
+    return "Hello HBNB!"
+
+"""A script that returns a specifeid string hbnb"""
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+"""A function that returns a specified string when returning the text in that directory."""
+@app.route("/c/<text>")
+def c_is_fun(text="is fun"):
+    return f"C" + text.replace('_', ' ')
+
+@app.route("/python/<text>")
+def python_is_cool(text="is cool"):
+    """Templates used to generate any type of text file.
+    """
+    return f"Python " + text.replace('_', ' ')
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def is_a_num(n):
+    return f"{n} is a number"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port="5000")
