05972bf167a0ad8d9ad5f8cc72c3cb78236d837e
mmothiba4 <mmothiba4@gmail.com>
2023-09-22 10:01:58 +0200

    python objects

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""model:state: contains the class definition of state. 
+"""model:state: contains the class definition of state.
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,4 +12,4 @@ class State(Base):
 
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128),nullable=False)
+    name = Column(String(128), nullable=False)
