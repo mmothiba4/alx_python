4fc3738303e1f1a73c416d3a4f23506285a2a7ff
mmothiba4 <mmothiba4@gmail.com>
2023-10-02 12:26:41 +0200

    API

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     todos = requests.get("https://jsonplaceholder.typicode.com/users/1/todos")
 
 filename = userId + '.csv'
-with open(filename, mode='w') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator='\n')
+with open(filename, mode='w') as f:
+    writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator='\n')
     for task in todos.json():
         if task.get('userId') == int(userId):
             writer.writerow([userId, name, str(task.get('completed')),task.get('title')])
