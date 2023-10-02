84707b01304899f78763bf3e868f5f7726776b00
mmothiba4 <mmothiba4@gmail.com>
2023-10-02 12:34:02 +0200

    API

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -5,16 +5,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    userId = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/1".format(userId))
-    username = user.json.get("username")
+    USER_ID = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/1".format(USER_ID))
+    USERNAME = user.json.get("USERNAME")
     todos = requests.get("https://jsonplaceholder.typicode.com/users/1/todos")
 
-filename = userId + '.csv'
+filename = USER_ID.csv
 with open(filename, mode='w') as f:
     writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL, lineterminator='\n')
     for task in todos.json():
-        if task.get('userId') == int(userId):
-            writer.writerow([userId, name, str(task.get('completed')),task.get('title')])
-
-if __name__ == "__main__":
+        if task.get('USER_ID') == int(USER_ID):
+            writer.writerow([USER_ID, name, str(task.get('TASK_COMPLETED_STATUS')),task.get('TASK_TITLE')])
