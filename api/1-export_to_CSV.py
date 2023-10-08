0251dd4f982f65c0dd4afc91980e5078b7cb1fd2
mmothiba4 <mmothiba4@gmail.com>
2023-10-08 20:18:07 +0200

    api

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-"""
-Check student .CSV output of user information
-"""
-
-import csv
-import requests
-import sys
-
-users_url = "https://jsonplaceholder.typicode.com/users?id="
-todos_url = "https://jsonplaceholder.typicode.com/todos"
-
-
-def user_info(id):
-    """ Check CSV formatting """
-
-    response = requests.get(todos_url).json()
-    with open(str(id) + ".csv", 'r') as f:
-        output = f.read().strip()
-        count = 0
-        flag = 0
-        for i in response:
-            if i['userId'] == id:
-                url = users_url + str(i['userId'])
-                usr_resp = requests.get(url).json()
-                line = '"' + str(i['userId']) + '","' + usr_resp[0]['username'] + '","' + str(i['completed']) + '","' + i['title'] + '"'
-                count += 1
-                if not line in output:
-                    print("Task {} Formatting: Incorrect".format(count))
-                    flag = 1
-
-    if flag == 0:
-        print("Formatting: OK")
-
-
 if __name__ == "__main__":
-    user_info(int(sys.argv[1]))
+
+    import csv
+    import requests
+    import sys
+
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    name = user.json().get('username')
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+
+    filename = userId + '.csv'
+    with open(filename, mode='w') as f:
+        writer = csv.writer(f, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL, lineterminator='\n')
+        for task in todos.json():
+            if task.get('userId') == int(userId):
+                writer.writerow([userId, name, str(task.get('completed')),
+                                 task.get('title')])
+
