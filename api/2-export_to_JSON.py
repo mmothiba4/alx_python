5aeb295067dfee04d55d944e8cc836cbdc695119
mmothiba4 <mmothiba4@gmail.com>
2023-10-08 20:19:30 +0200

    api

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,45 +1,30 @@
 #!/usr/bin/python3
 
-# A script to export data in the JSON format.
+"""Exports data in the JSON format"""
 
-import json
-import requests
-import sys
-
-# No execution of this file when imported
 if __name__ == "__main__":
-    
-# Pass employee id on command line
-    id = sys.argv[1]
 
-# APIs 
-    userTodoURL = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-    userProfile = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    import json
+    import requests
+    import sys
 
-# Make requests on APIs
-    todoResponse = requests.get(userTodoURL)
-    profileResponse = requests.get(userProfile)
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    todos = todos.json()
 
-# Parse responses and store in variables
-    todoJson_Data = todoResponse.json()
-    profileJson_Data = profileResponse.json()
+    todoUser = {}
+    taskList = []
 
-#Get employee information
-    employeeName = profileJson_Data['username']
+    for task in todos:
+        if task.get('userId') == int(userId):
+            taskDict = {"task": task.get('title'),
+                        "completed": task.get('completed'),
+                        "username": user.json().get('username')}
+            taskList.append(taskDict)
+    todoUser[userId] = taskList
 
-    dataList = []# Empty list to store the dictionaries
-
-    for data in todoJson_Data:
-        dataDict = {"task":data['title'], "completed":data['completed'], "username":employeeName}
-        dataList.append(dataDict)
-
-# A dictionary of list of dictionaries to be exported to JSON
-    outputData = {profileJson_Data['id']: dataList}
-
-# Specify the JSON file path
-    json_file_path = '{}.json'.format(todoJson_Data[0]['userId'])
-
-# Open the JSON file in write mode
-    with open(json_file_path, 'w') as json_file:
-    # Serialize and write the data to the JSON file
-        json.dump(outputData, json_file)
+    filename = userId + '.json'
+    with open(filename, mode='w') as f:
+        json.dump(todoUser, f)
